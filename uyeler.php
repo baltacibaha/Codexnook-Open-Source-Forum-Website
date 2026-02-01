5ee504d366a2b9332574704a199d8643bacb2e77
Baha Baltacı <95427016+baltacibaha@users.noreply.github.com>
2026-02-01 19:05:24 +0300

    Revise uyeler.php for improved structure and styling
    Refactor the layout and styling of the members page.

--- uyeler.php
+++ uyeler.php
@@ -7,48 +7,41 @@ include 'func.php';
 
 <!DOCTYPE html>
 <html>
-<link rel="stylesheet" href="main.css" />
 
 <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3809740976001286"
-        crossorigin="anonymous"></script>
     <title>Üyelerimiz</title>
     <link rel="shortcut icon" href="icon.png" type="image/x-icon" />
+    <link rel="stylesheet" href="main.css" />
 </head>
 
 <body>
-    <center>
-        <?php include 'header.php'; ?>
-        <br><br>
-        <center>
-            <table>
-                <tr>
-                    <td colspan="2">
-                        <h2>Üyelerimiz:</h2>
-                        <style>
-                         {
-                             text-decoration:none;
-                         }
-                        </style>
+    <?php include 'header.php'; ?>
+    
+    <div class="uyeler-container">
+        <div class="uyeler-box">
+            <h2>Üyelerimiz</h2>
+            <ul class="uyeler-liste">
+                <?php
+                $dataList = $db->prepare("SELECT * FROM uyeler");
+                $dataList->execute();
+                $dataList = $dataList->fetchAll(PDO::FETCH_ASSOC);
 
-                        <ul>
-                            <?php
-                            $dataList = $db->prepare("SELECT * FROM uyeler");
-                            $dataList->execute();
-                            $dataList = $dataList->fetchAll(PDO::FETCH_ASSOC);
+                foreach ($dataList as $row) {
+                    echo '<li class="uye-item">
+                            <a href="profil.php?kadi=' . htmlspecialchars($row["uye_kadi"]) . '">
+                                <span class="uye-icon">👤</span>
+                                <span class="uye-isim">' . htmlspecialchars($row["uye_adsoyad"]) . '</span>
+                                <span class="uye-arrow">→</span>
+                            </a>
+                          </li>';
+                }
+                ?>
+            </ul>
+        </div>
+    </div>
 
-                            foreach ($dataList as $row) {
-                                echo '<li><a href="profil.php?kadi=' . $row["uye_kadi"] . '">' . $row["uye_adsoyad"] . '</a></li>';
-                            }
-                            ?>
-                        </ul>
-                    </td>
-                </tr>
-            </table>
-        </center>
-    </center>
 </body>
 </html>
