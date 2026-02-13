17eb4e34cbcbcb35b6c0093650e04f90fff87ca4
Baha Baltacı <95427016+baltacibaha@users.noreply.github.com>
2026-02-13 15:43:26 +0300

    Fix HTML structure and update page title

--- kategori.php
+++ kategori.php
@@ -8,7 +8,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3809740976001286"
      crossorigin="anonymous"></script>
-    <title>CodexNook.com/Kategori</title>
+    <title>Forum/Kategori</title>
   <link rel="shortcut icon" href="icon.png" type="image/x-icon" />
 </head>
 
@@ -62,4 +62,5 @@
 
             ?>
         </ul>
+
     </center>
